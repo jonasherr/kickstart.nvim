184ffe271cb5e424ce825921c57a4a70cdf44878
Jonas Herrmannsdörfer <jonas@herrmannsdoerfer.dev>
2024-01-19 15:13:09 +0100

    added terminal

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -171,5 +171,16 @@ return {
 			})
 		end
 	},
-
+	{
+		'akinsho/toggleterm.nvim',
+		version = "*",
+		config = true,
+		keys = {
+			{ "<leader>ot",   ":ToggleTerm<CR>",            silent = true, desc = 'Toggle terminal',        mode = { "n", "v" } },
+			{ "<leader>ott",  ":2ToggleTerm<CR>",           silent = true, desc = 'Toggle terminal 2',      mode = { "n", "v" } },
+			{ "<leader>ottt", ":3ToggleTerm<CR>",           silent = true, desc = 'Toggle terminal 3',      mode = { "n", "v" } },
+			{ "<leader>ots",  ":TermSelect<CR>",            silent = true, desc = 'Toggle terminal select', mode = { "n", "v" } },
+			{ "<C-t>",        "<C-\\><C-n>:ToggleTerm<CR>", silent = true, noremap = true,                  desc = 'Hide terminal', mode = { "t" } }
+		}
+	}
 }
